c547643924cae9acfa99d046c8a068669784e4d8
milk717 <robolindasoo@gmail.com>
2021-09-16 10:26:58 +0900

    3-3_환율앱_강의자료대로

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,41 +2,33 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.databinding.ActivityMainBinding
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
+        fun onBtnClicked(view: View){
+            val dollars = findViewById<EditText>(R.id.dollarsInput)
+            val txt = findViewById<TextView>(R.id.result_text)
+            if(dollars.text.isNotEmpty()){
+                val dollarValue = dollars.text.toString().toFloat()
+                val wonValue = dollarValue * 1136
 
-
-        binding.btnName.setOnClickListener{
-
-            //백그라운드 색상 설정
-            //val color = ContextCompat.getColor(this,R.color.custom)
-            //binding.background.setBackgroundColor(color)
-
-            val dollarsInput = binding.dollarsInput
-            val resultText = binding.resultText
-            val str = getString(R.string.exchange_won)
-
-            val dp = resources.getDimension(R.dimen.textSize)
-
-
-            if(dollarsInput.text.isNotEmpty()){     //비어있지 않을 때
-                resultText.setTextSize(TypedValue.COMPLEX_UNIT_DIP,20f)
-                var dollar = dollarsInput.text.toString().toFloat()
-                var won = dollar*1136
-                resultText.text = "$str: $won 원"
+                val color = ContextCompat.getColor(this, R.color.custom)
+                val dp = resources.getDimension(R.dimen.textSize)
+                txt.setTextSize(TypedValue.COMPLEX_UNIT_DIP,dp)
+                txt.setTextColor(color)
+                txt.text = wonValue.toString()
             }
             else{
-                resultText.setTextSize(TypedValue.COMPLEX_UNIT_PX,dp)
-                resultText.text = "No Value"
-                //텍스트 사이즈 설정
-
+                txt.text = "No Value"
             }
         }
     }
