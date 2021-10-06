cf5933794db855d0f5f1389c44ef23f0d51b6db8
milk717 <robolindasoo@gmail.com>
2021-10-07 08:40:00 +0900

    4-1_Practice #1 TextView의 constraint을 없앤 후 다시 constraints을설정해 보자!

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.TypedValue
-import android.view.View
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,23 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fun onBtnClicked(view: View){
-            val dollars = findViewById<EditText>(R.id.dollarsInput)
-            val txt = findViewById<TextView>(R.id.result_text)
-            if(dollars.text.isNotEmpty()){
-                val dollarValue = dollars.text.toString().toFloat()
-                val wonValue = dollarValue * 1136
-
-                val color = ContextCompat.getColor(this, R.color.custom)
-                val dp = resources.getDimension(R.dimen.textSize)
-                txt.setTextSize(TypedValue.COMPLEX_UNIT_DIP,dp)
-                txt.setTextColor(color)
-                txt.text = wonValue.toString()
-            }
-            else{
-                txt.text = "No Value"
-            }
-        }
     }
 
 }
