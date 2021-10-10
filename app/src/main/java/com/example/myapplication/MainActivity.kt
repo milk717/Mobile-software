81e11bfd7432da5ac404d5d672418141827d742f
milk717 <robolindasoo@gmail.com>
2021-10-10 16:25:40 +0900

    4-2_table_layout

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,17 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    fun onButtonClicked(view: View){
-        var iv: ImageView = findViewById(R.id.imageView)
-        var iv2: ImageView = findViewById(R.id.imageView2)
 
-        if(toggleImage){
-            iv.visibility = View.VISIBLE
-            iv2.visibility = View.INVISIBLE
-        }else{
-            iv.visibility = View.INVISIBLE
-            iv2.visibility = View.VISIBLE
-        }
-        toggleImage = !toggleImage
-    }
 }
