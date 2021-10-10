4dc7de4cff2f5e3f0b8acda5965179c6bd555fe7
milk717 <robolindasoo@gmail.com>
2021-10-10 16:16:15 +0900

    4-2_버튼눌러서 이미지 바꾸기

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var toggleImage:Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
+    fun onButtonClicked(view: View){
+        var iv: ImageView = findViewById(R.id.imageView)
+        var iv2: ImageView = findViewById(R.id.imageView2)
 
+        if(toggleImage){
+            iv.visibility = View.VISIBLE
+            iv2.visibility = View.INVISIBLE
+        }else{
+            iv.visibility = View.INVISIBLE
+            iv2.visibility = View.VISIBLE
+        }
+        toggleImage = !toggleImage
+    }
 }
