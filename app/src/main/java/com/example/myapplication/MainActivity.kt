9e6fa06e2d638cc8824165932a3b0c6a3e3fc5f9
milk717 <robolindasoo@gmail.com>
2021-10-12 21:17:51 +0900

    5_버튼 누르면 토스트 메시지_inner

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,17 +12,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //메소드 안에 클래스를 생성하는 것 : 중첩 클래스
-        class BtnListener : View.OnClickListener{
-            override fun onClick(v: View?){
-                //그냥 this 사용하면 안됨, BtnListener 는 UI를 구현하는 클래스가 아니어서
-                //(엑티비티 컴포넌트를 구현하고 있는 객체, 문자열, 출력 시간 설정)
-                Toast.makeText(this@MainActivity,"버튼이 눌렸습니다",Toast.LENGTH_SHORT).show()
-            }
-        }
-
         var btnView: Button = findViewById(R.id.btnView)
         //val lis = BtnListener()
         btnView.setOnClickListener(BtnListener())        //괄호 안에 리스너 객체 등록시켜줘야함
+    }
+
+    inner class BtnListener : View.OnClickListener{     //inner : 클래스 바깥쪽에 있는 속성들 사용 가능
+        override fun onClick(v: View?){
+            //그냥 this 사용하면 안됨, BtnListener 는 UI를 구현하는 클래스가 아니어서
+            //(엑티비티 컴포넌트를 구현하고 있는 객체, 문자열, 출력 시간 설정)
+            Toast.makeText(applicationContext,"버튼이 눌렸습니다",Toast.LENGTH_SHORT).show()
+        }
     }
 }
