8d349025b9b2278ab5fa46bef6b9abe94154d8f0
milk717 <robolindasoo@gmail.com>
2021-10-14 21:54:47 +0900

    6_단위변환_log

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,55 +1,25 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
-import android.widget.TextView
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity(){
 
+    private val TAG = "Unit Conversion"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val wholeLayout: ConstraintLayout = findViewById(R.id.whole_layout)
-        wholeLayout.setOnTouchListener(object : View.OnTouchListener{
-            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                handleTouch(event)
-                return true
-            }
-        })
+        val scale:Float =resources.displayMetrics.density
+        Log.i(TAG,"density = $scale") //비율 : px = dp = scale
+
+        var dpi = (scale*160).toInt()
+        Log.i(TAG,"dpi = $dpi")
+
+        val dp = 16
+        var px = (scale*dp+0.5).toInt() //정수는 1.7 -> 1, 1.7 + 0.5 -> 2.2 ->2
+        Log.i(TAG,"dp = $dp, px = $px")
     }
-
-    private fun handleTouch(m:MotionEvent?){        //세이프콜 해줘야함: onTouch에서 전달하는 값이 null일수도 있어서
-        if (m == null) return
-        val pointerCount:Int = m.pointerCount
-        var textView:TextView = findViewById(R.id.textView)
-        var textView2:TextView = findViewById(R.id.textView2)
-
-        for (i in 0 until pointerCount){
-            val x:Float = m.getX(i)
-            val y:Float = m.getY(i)
-            val id:Int = m.getPointerId(i)
-            val actionIndex:Int = m.actionIndex
-            var actionString:String = ""
-
-            when(m.action){
-                MotionEvent.ACTION_DOWN -> actionString = "Down"
-                MotionEvent.ACTION_UP -> actionString = "UP"
-                MotionEvent.ACTION_POINTER_UP -> actionString = "POINTER_UP"
-                MotionEvent.ACTION_POINTER_DOWN -> actionString = "POINTER_DOWN"
-                MotionEvent.ACTION_MOVE -> actionString = "MOVE"
-            }
-            var message:String = "Action $actionString,Index:$actionIndex, ID:$id"
-            if(id == 0)
-                textView.text = outMessage(message,x,y)
-            else
-                textView2.text = outMessage(message,x,y)
-
-        }
-    }
-    private fun outMessage(msg:String, x:Float, y:Float) = "%s : %.2f %.2f".format(msg,x,y)
 }
