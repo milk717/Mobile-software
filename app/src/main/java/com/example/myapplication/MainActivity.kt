718a868cde9cb5ca8361e11794416ea1a92c946a
milk717 <robolindasoo@gmail.com>
2021-10-14 21:39:23 +0900

    5_멀티터치

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,39 +5,51 @@ import android.view.MotionEvent
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity(){
 
-    private var sb = StringBuilder()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myView: View = findViewById(R.id.myView)
-        myView.setOnTouchListener { _, event ->         //두개의 파라미터가 있을 때는 사용되지 않아도 생략은 불가능 '_'로 하기
-            handleTouch(event)
-            true     //확실하게 처리했다.
-        }
+        val wholeLayout: ConstraintLayout = findViewById(R.id.whole_layout)
+        wholeLayout.setOnTouchListener(object : View.OnTouchListener{
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                handleTouch(event)
+                return true
+            }
+        })
     }
 
     private fun handleTouch(m:MotionEvent?){        //세이프콜 해줘야함: onTouch에서 전달하는 값이 null일수도 있어서
+        if (m == null) return
+        val pointerCount:Int = m.pointerCount
         var textView:TextView = findViewById(R.id.textView)
-        if(m == null) return
-        var x:Float = m.x
-        var y:Float = m.y
+        var textView2:TextView = findViewById(R.id.textView2)
 
-        when(m.action){
-            MotionEvent.ACTION_DOWN -> outMessage("손가락을 누름" ,x,y)
-            MotionEvent.ACTION_UP -> outMessage("손가락을 뗌",x,y)
-            MotionEvent.ACTION_MOVE -> outMessage("손가락을 이동" ,x,y)
-            else -> outMessage("액션 없음",x,y)
+        for (i in 0 until pointerCount){
+            val x:Float = m.getX(i)
+            val y:Float = m.getY(i)
+            val id:Int = m.getPointerId(i)
+            val actionIndex:Int = m.actionIndex
+            var actionString:String = ""
+
+            when(m.action){
+                MotionEvent.ACTION_DOWN -> actionString = "Down"
+                MotionEvent.ACTION_UP -> actionString = "UP"
+                MotionEvent.ACTION_POINTER_UP -> actionString = "POINTER_UP"
+                MotionEvent.ACTION_POINTER_DOWN -> actionString = "POINTER_DOWN"
+                MotionEvent.ACTION_MOVE -> actionString = "MOVE"
+            }
+            var message:String = "Action $actionString,Index:$actionIndex, ID:$id"
+            if(id == 0)
+                textView.text = outMessage(message,x,y)
+            else
+                textView2.text = outMessage(message,x,y)
+
         }
-        textView.text = sb.toString()
     }
-    private fun outMessage(msg:String, x:Float, y:Float){
-        var textView:TextView = findViewById(R.id.textView)
-        sb.append("%-10s: %.2f %.2f\n".format(msg,x,y))
-        textView.text = sb.toString()
-    }
+    private fun outMessage(msg:String, x:Float, y:Float) = "%s : %.2f %.2f".format(msg,x,y)
 }
