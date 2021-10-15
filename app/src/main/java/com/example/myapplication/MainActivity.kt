d2d4859b86e60e8a0e8750f5d063a91e8922bb47
milk717 <robolindasoo@gmail.com>
2021-10-15 11:30:47 +0900

    6_키보드입력

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(){
@@ -8,5 +13,15 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val srchEditText:EditText = findViewById(R.id.editText)
+        srchEditText.setOnEditorActionListener(object : TextView.OnEditorActionListener{
+            override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                if (actionId == EditorInfo.IME_ACTION_SEND){
+                    Toast.makeText(applicationContext,"탐색 문자열은 ${srchEditText.text}",Toast.LENGTH_SHORT).show()
+                }
+                return false
+            }
+        })
     }
 }
