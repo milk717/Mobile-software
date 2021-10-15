43c1e5017b9741c61b984588a449854fcd0c8d26
milk717 <robolindasoo@gmail.com>
2021-10-15 11:33:14 +0900

    6_키보드입력_람다

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,13 +13,16 @@ class MainActivity : AppCompatActivity(){
         setContentView(R.layout.activity_main)
 
         val srchEditText:EditText = findViewById(R.id.editText)
-        srchEditText.setOnEditorActionListener(object : TextView.OnEditorActionListener{
-            override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
-                if (actionId == EditorInfo.IME_ACTION_SEND){
-                    Toast.makeText(applicationContext,"탐색 문자열은 ${srchEditText.text}",Toast.LENGTH_SHORT).show()
-                }
-                return false
+        srchEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEND) {
+                Toast.makeText(
+                    applicationContext,
+                    "탐색 문자열은 ${srchEditText.text}",
+                    Toast.LENGTH_SHORT
+                ).show()
+                true
             }
-        })
+            false
+        }
     }
 }
