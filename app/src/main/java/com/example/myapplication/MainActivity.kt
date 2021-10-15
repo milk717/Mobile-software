221bde48d23e218665caa90b1367c0de21e59109
milk717 <robolindasoo@gmail.com>
2021-10-15 11:12:06 +0900

    6_BitmapButton

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,25 +1,12 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(){
 
-    private val TAG = "Unit Conversion"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val scale:Float =resources.displayMetrics.density
-        Log.i(TAG,"density = $scale") //비율 : px = dp = scale
-
-        var dpi = (scale*160).toInt()
-        Log.i(TAG,"dpi = $dpi")
-
-        val dp = 16
-        var px = (scale*dp+0.5).toInt() //정수는 1.7 -> 1, 1.7 + 0.5 -> 2.2 ->2
-        Log.i(TAG,"dp = $dp, px = $px")
     }
 }
