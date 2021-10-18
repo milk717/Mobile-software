29f5c328d648251d0cd86793f05a2093f93a6df4
milk717 <robolindasoo@gmail.com>
2021-10-18 19:47:05 +0900

    6_ratingBar_button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.RatingBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -11,11 +13,21 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val ratingBarListener =
-            RatingBar.OnRatingBarChangeListener { _, rating, _ -> showToast("평점은 $rating") }
+//        val ratingBarListener =
+//            RatingBar.OnRatingBarChangeListener { _, rating, _ ->
+//                showToast("평점은 $rating")
+//            }
+//
+          val ratingBar: RatingBar = findViewById(R.id.ratingBar)
+//        ratingBar.onRatingBarChangeListener = ratingBarListener
 
-        val ratingBar: RatingBar = findViewById(R.id.ratingBar)
-        ratingBar.onRatingBarChangeListener = ratingBarListener
+        val button: Button = findViewById(R.id.button)  //버튼을 불러오고
+        button.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                var rating = ratingBar.rating.toString()
+                showToast("평점은 $rating")
+            }
+        })
 
     }
     fun showToast(msg: String) {
