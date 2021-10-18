3b32d2d1b99a6df51d6b270e65350e16b5d3dacb
milk717 <robolindasoo@gmail.com>
2021-10-18 19:31:59 +0900

    6_ratingBar

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.RadioButton
-import android.widget.RadioGroup
+import android.widget.RatingBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,30 +11,14 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var sb = StringBuilder()
-        val radioGroup:RadioGroup = findViewById(R.id.radioGroup)
-        radioGroup.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener{
-            override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
-                sb.delete(0,sb.length)
-                var radioButton:RadioButton = findViewById(checkedId)
-                sb.append(radioButton.text)
-                sb.append("를 선택했군요.")
-            }
-        })
+        val ratingBarListener =
+            RatingBar.OnRatingBarChangeListener { _, rating, _ -> showToast("평점은 $rating") }
 
-        var id:Int =radioGroup.checkedRadioButtonId
-        when(id){
-            R.id.red->showToast("Red 가 기본 선택")
-            R.id.blue->showToast("blue 가 기본 선택")
-            R.id.green->showToast("green 가 기본 선택")
-        }
+        val ratingBar: RatingBar = findViewById(R.id.ratingBar)
+        ratingBar.onRatingBarChangeListener = ratingBarListener
 
-        val btn:Button = findViewById(R.id.button)
-        btn.setOnClickListener{
-            showToast(sb.toString())
-        }
     }
-    fun showToast(msg:String){
-        Toast.makeText(applicationContext,msg,Toast.LENGTH_SHORT).show()
+    fun showToast(msg: String) {
+        Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show()
     }
 }
