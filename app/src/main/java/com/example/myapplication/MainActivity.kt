6d536061d03a5440639f854100e0fa2602fe813e
milk717 <robolindasoo@gmail.com>
2021-10-27 14:26:41 +0900

    중간고사(A)_2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity(){
 
+    private var toggleImage = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        val imageView: ImageView =findViewById(R.id.nightImageView)
+        val textView: TextView = findViewById(R.id.desTextView)
+        imageView.setOnClickListener {
+            if (toggleImage) {
+                textView.visibility = View.VISIBLE
+            } else {
+                textView.visibility = View.INVISIBLE
+            }
+            toggleImage = !toggleImage
+        }
     }
 }
