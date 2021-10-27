6f326dcb95c52d90b4f6571dc191f36d7915f88f
milk717 <robolindasoo@gmail.com>
2021-10-27 14:44:32 +0900

    중간고사(A)_3,4

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,28 +1,41 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity(){
 
-    private var toggleImage = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val imageView: ImageView =findViewById(R.id.nightImageView)
-        val textView: TextView = findViewById(R.id.desTextView)
-        imageView.setOnClickListener {
-            if (toggleImage) {
-                textView.visibility = View.VISIBLE
-            } else {
-                textView.visibility = View.INVISIBLE
+
+        val btn: Button = findViewById(R.id.button)
+        val radioGroup: RadioGroup = findViewById(R.id.radioGroup)
+        val editText: EditText = findViewById(R.id.editText)
+        val editText2:EditText = findViewById(R.id.editText1)
+        var res:String = ""     //계산 값을 문자열로 바꿔 저장할 변수
+
+        radioGroup.setOnCheckedChangeListener(object: RadioGroup.OnCheckedChangeListener{
+            override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+                var resFloat:Float = 1.0f
+                var str:String = editText.text.toString()
+                when(val radioId = radioGroup.checkedRadioButtonId){
+                    R.id.radioButton -> resFloat = ((str.toFloat() * 1.8f) + 32)       //섭씨 화씨 체크된 경우
+                    R.id.radioButton2 -> resFloat = ((str.toFloat() - 32) / 1.8f)      //화씨 섭씨 체크된 경우
+                    else->resFloat = 1.0f
+                }
+                res = resFloat.toString()
             }
-            toggleImage = !toggleImage
+        })
+
+        btn.setOnClickListener {        //버튼 누르면
+            editText2.setText(res)      //값 표시
         }
     }
+
 }
+
