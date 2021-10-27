f12c712453c6e04b994d081a4972068a21b9a87b
milk717 <robolindasoo@gmail.com>
2021-10-27 11:13:12 +0900

    7_callBackMethod

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,31 +13,36 @@ class MainActivity : AppCompatActivity(){
         Log.d(TAG, "onCreate()호출")
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState()호출")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d(TAG, "onRestoreInstanceState()호출")
+    }
+
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart()호출")
     }
-
     override fun onRestart() {
         super.onRestart()
         Log.d(TAG, "onRestart()호출")
     }
-
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume()호출")
     }
-
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause()호출")
     }
-
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop()호출")
     }
-
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy()호출")
