32a010412b51e174935a3eb3b8ad19fbe68af728
milk717 <robolindasoo@gmail.com>
2021-10-28 13:35:40 +0900

    7_dialog_목록선택

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,21 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val callButton: Button = findViewById(R.id.callButton)
+        //        타입은 배열인데 <String>타입의 배열이다.
+        val items:Array<String> = resources.getStringArray(R.array.colors)
 
+        val callButton: Button = findViewById(R.id.callButton)
         callButton.setOnClickListener {     //Context 와 Activity 를 구분해서 생각해야함 (아직잘 모르겠음)
             AlertDialog.Builder(this@MainActivity)        //this 라고만 쓰면 나중에 에러남
                 .setTitle("확인 메시지")     //다이얼로그 제목 지정
-                .setMessage("앱을 종료하겠습니까?")   //다이얼로그 메시지 출력
                 .setIcon(R.drawable.ic_baseline_thumb_up_alt_24)      //다이얼로그 타이틀 아이콘 설정
-                .setPositiveButton("예") { _, _ ->
-                    finish()
-                }
-                .setNeutralButton("취소") { _, _ ->
-                    Toast.makeText(applicationContext, "명령을 취소하였습니다.", Toast.LENGTH_SHORT).show()
-                }
-                .setNegativeButton("아니요") { _, _ ->
-                    Toast.makeText(applicationContext, "앱을 계속 사용합니다", Toast.LENGTH_SHORT).show()
+                //.setMessage("색 선택")       //이거 안지우면 아래에 setItems 안뜸
+                .setItems(items) { _, which ->
+                    Toast.makeText(
+                        applicationContext,
+                        "$which, ${items[which]}가 선택됨", Toast.LENGTH_SHORT
+                    ).show()
                 }
                 .show()      //이거 안하면 안보임 (Toast 의 show 생각)
         }
