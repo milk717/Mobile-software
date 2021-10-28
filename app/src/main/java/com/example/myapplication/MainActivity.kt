a41acde4451f831389eb48f1c56aac5def560415
milk717 <robolindasoo@gmail.com>
2021-10-28 13:59:21 +0900

    7_dialog_setSingleChoiceItems

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,21 +13,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //        타입은 배열인데 <String>타입의 배열이다.
         val items:Array<String> = resources.getStringArray(R.array.colors)
+        var mSelect = -1
 
         val callButton: Button = findViewById(R.id.callButton)
-        callButton.setOnClickListener {     //Context 와 Activity 를 구분해서 생각해야함 (아직잘 모르겠음)
-            AlertDialog.Builder(this@MainActivity)        //this 라고만 쓰면 나중에 에러남
-                .setTitle("확인 메시지")     //다이얼로그 제목 지정
-                .setIcon(R.drawable.ic_baseline_thumb_up_alt_24)      //다이얼로그 타이틀 아이콘 설정
-                //.setMessage("색 선택")       //이거 안지우면 아래에 setItems 안뜸
-                .setItems(items) { _, which ->
+        callButton.setOnClickListener {
+            AlertDialog.Builder(this@MainActivity)
+                .setTitle("확인 메시지")
+                .setIcon(R.drawable.ic_baseline_thumb_up_alt_24)
+
+                    //두번째 파라미터 -1하면 아무것도 선택 안된 초기상태
+                .setSingleChoiceItems(items, mSelect) { _, which -> //여기서 which 는 어떤 것이 선택되었는지 인덱스 값을 나타내고
+                    mSelect = which
+                    Log.d("확인", "$mSelect, $which")
+                }
+                .setPositiveButton("선택") { _, which ->  //여기서 which 는 -1임, 즉 SingleChoiceItems 에서의 which 값과 다름.
+                                                                //자세한건 아직 모르겠음. 그래서 items[which]하면 에러남
                     Toast.makeText(
                         applicationContext,
-                        "$which, ${items[which]}가 선택됨", Toast.LENGTH_SHORT
+                        "$which, ${items[mSelect]}가 선택됨", Toast.LENGTH_SHORT
                     ).show()
+                    Log.d("확인", "$mSelect, $which")
                 }
+                .setNegativeButton("취소",null)
                 .show()      //이거 안하면 안보임 (Toast 의 show 생각)
         }
     }
