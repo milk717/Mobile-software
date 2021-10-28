b8e27e30c6c8063df70298f49701cbeb02b3f4ca
milk717 <robolindasoo@gmail.com>
2021-10-28 14:14:05 +0900

    7_dialog_setMultiChoiceItems

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.myapplication
 
+import android.content.DialogInterface
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val items:Array<String> = resources.getStringArray(R.array.colors)
-        var mSelect = -1
+        var checked:Array<Boolean> = arrayOf(false, false, false)
 
         val callButton: Button = findViewById(R.id.callButton)
         callButton.setOnClickListener {
@@ -22,18 +22,27 @@ class MainActivity : AppCompatActivity() {
                 .setTitle("확인 메시지")
                 .setIcon(R.drawable.ic_baseline_thumb_up_alt_24)
 
-                    //두번째 파라미터 -1하면 아무것도 선택 안된 초기상태
-                .setSingleChoiceItems(items, mSelect) { _, which -> //여기서 which 는 어떤 것이 선택되었는지 인덱스 값을 나타내고
-                    mSelect = which
-                    Log.d("확인", "$mSelect, $which")
-                }
-                .setPositiveButton("선택") { _, which ->  //여기서 which 는 -1임, 즉 SingleChoiceItems 에서의 which 값과 다름.
-                                                                //자세한건 아직 모르겠음. 그래서 items[which]하면 에러남
+                //두번째 파라미터에 checked 넣으면 에러남. null 넣기
+                //아래는 두번째 멀티초이스 파라미터에 대한 안드로이드 설명
+                //checkedItems – 체크할 항목을 지정합니다. null 이어야 하며, 이 경우 체크된 항목이 없습니다.
+                // null 이 아닌 경우 항목 배열과 길이가 같아야 합니다.
+                .setMultiChoiceItems(items, null ,object: DialogInterface.OnMultiChoiceClickListener{
+                    override fun onClick(dialog: DialogInterface?, which: Int, isChecked: Boolean) {
+                        checked[which] = isChecked       //선택된값 == which 를 체크여부표시 == isChecked
+                    }
+                })
+                .setPositiveButton("선택"){ _, _ ->
+                    var sb = StringBuilder()
+                    //indices == 배열에 대해 유효한 인덱스 범위 반환
+                    for (i in checked.indices){
+                        if(checked[i]){
+                            sb.append("${items[i]} ")
+                        }
+                    }
                     Toast.makeText(
                         applicationContext,
-                        "$which, ${items[mSelect]}가 선택됨", Toast.LENGTH_SHORT
+                        "$sb 가 선택됨", Toast.LENGTH_SHORT
                     ).show()
-                    Log.d("확인", "$mSelect, $which")
                 }
                 .setNegativeButton("취소",null)
                 .show()      //이거 안하면 안보임 (Toast 의 show 생각)
