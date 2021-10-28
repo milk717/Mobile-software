f8c604c1e5d2203537655a688bc40770a73b9fdf
milk717 <robolindasoo@gmail.com>
2021-10-28 13:01:04 +0900

    7_dialog_basicAlertDialog

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,8 +2,7 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -13,29 +12,16 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn: Button = findViewById(R.id.button)
-        val radioGroup: RadioGroup = findViewById(R.id.radioGroup)
-        val editText: EditText = findViewById(R.id.editText)
-        val editText2:EditText = findViewById(R.id.editText1)
-        var res:String = ""     //계산 값을 문자열로 바꿔 저장할 변수
+        val callButton:Button = findViewById(R.id.callButton)
 
-        radioGroup.setOnCheckedChangeListener(object: RadioGroup.OnCheckedChangeListener{
-            override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
-                var resFloat:Float = 1.0f
-                var str:String = editText.text.toString()
-                when(val radioId = radioGroup.checkedRadioButtonId){
-                    R.id.radioButton -> resFloat = ((str.toFloat() * 1.8f) + 32)       //섭씨 화씨 체크된 경우
-                    R.id.radioButton2 -> resFloat = ((str.toFloat() - 32) / 1.8f)      //화씨 섭씨 체크된 경우
-                    else->resFloat = 1.0f
-                }
-                res = resFloat.toString()
-            }
-        })
-
-        btn.setOnClickListener {        //버튼 누르면
-            editText2.setText(res)      //값 표시
+        callButton.setOnClickListener {     //Context 와 Activity 를 구분해서 생각해야함 (아직잘 모르겠음)
+            val builder = AlertDialog.Builder(this@MainActivity)        //this 라고만 쓰면 나중에 에러남
+                                                //괄호 안에 applicationContext 를 사용하면 runtime error 발생
+            builder.setTitle("게시판")     //다이얼로그 제목 지정
+            builder.setMessage("2학년을 위한 환영회")   //다이얼로그 메시지 출력
+            builder.setIcon(R.drawable.ic_launcher_foreground)      //다이얼로그 타이틀 아이콘 설정
+            builder.show()      //이거 안하면 안보임 (Toast 의 show 생각)
         }
     }
-
 }
 
