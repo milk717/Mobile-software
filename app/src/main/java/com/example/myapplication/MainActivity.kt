8f64ef8fc8044ade6c7a082f84ced424400bf693
milk717 <robolindasoo@gmail.com>
2021-10-28 23:20:22 +0900

    7_Snackbar

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.myapplication
 
-import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,40 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val items:Array<String> = resources.getStringArray(R.array.colors)
-        var checked:Array<Boolean> = arrayOf(false, false, false)
-
-        val callButton: Button = findViewById(R.id.callButton)
-        callButton.setOnClickListener {
-            AlertDialog.Builder(this@MainActivity)
-                .setTitle("확인 메시지")
-                .setIcon(R.drawable.ic_baseline_thumb_up_alt_24)
-
-                //두번째 파라미터에 checked 넣으면 에러남. null 넣기
-                //아래는 두번째 멀티초이스 파라미터에 대한 안드로이드 설명
-                //checkedItems – 체크할 항목을 지정합니다. null 이어야 하며, 이 경우 체크된 항목이 없습니다.
-                // null 이 아닌 경우 항목 배열과 길이가 같아야 합니다.
-                .setMultiChoiceItems(items, null ,object: DialogInterface.OnMultiChoiceClickListener{
-                    override fun onClick(dialog: DialogInterface?, which: Int, isChecked: Boolean) {
-                        checked[which] = isChecked       //선택된값 == which 를 체크여부표시 == isChecked
-                    }
-                })
-                .setPositiveButton("선택"){ _, _ ->
-                    var sb = StringBuilder()
-                    //indices == 배열에 대해 유효한 인덱스 범위 반환
-                    for (i in checked.indices){
-                        if(checked[i]){
-                            sb.append("${items[i]} ")
-                        }
-                    }
-                    Toast.makeText(
-                        applicationContext,
-                        "$sb 가 선택됨", Toast.LENGTH_SHORT
-                    ).show()
-                }
-                .setNegativeButton("취소",null)
-                .show()      //이거 안하면 안보임 (Toast 의 show 생각)
+        val callbackButton: Button = findViewById(R.id.callbackButton)
+        //View 는 제일 상위 layout
+        val mainLayout: View = findViewById(R.id.main_layout_id)
+        callbackButton.setOnClickListener {
+            showSnackbar(mainLayout)
         }
+    }
+    private fun showSnackbar(view:View){
+        val mySnackbar:Snackbar = Snackbar.make(view,
+        "메시지를 삭제할까요?",Snackbar.LENGTH_SHORT)
+        mySnackbar.setAction("취소"){
+            mySnackbar.dismiss()        //스낵바 사라지게
+        }
+        mySnackbar.show()
     }
 }
 
