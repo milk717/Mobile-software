6b6ae200eeef75e4cba57b1cae23fc3bad509259
milk717 <robolindasoo@gmail.com>
2021-11-08 11:53:12 +0900

    7_Options Menu

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,52 @@
 package com.example.myapplication
 
-import android.content.Intent
-import android.net.Uri
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.Button
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var layoutView: ConstraintLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.callButton).setOnClickListener {
-            val intent = Uri.parse("tel:(+82)01012345678").let{ number->
-                Intent(Intent.ACTION_DIAL, number)
+        layoutView = findViewById(R.id.layoutView)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+        when(item.itemId){
+            R.id.menu_apple->{
+                layoutView.setBackgroundColor(Color.RED)
+                item.isChecked = false
+                showToast(item.title.toString())
+                true
             }
-            startActivity(intent)
+            R.id.menu_graph->{
+                layoutView.setBackgroundColor(Color.MAGENTA)
+                item.isChecked = false
+                showToast(item.title.toString())
+                true
+            }
+            R.id.menu_banana->{
+                layoutView.setBackgroundColor(Color.YELLOW)
+                item.isChecked = false
+                showToast(item.title.toString())
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
+
+    private fun showToast(msg:String){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
     }
 }
 
