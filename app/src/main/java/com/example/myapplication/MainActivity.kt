92311c7b235f2293a30f0fc2d98aeb76083d1394
milk717 <robolindasoo@gmail.com>
2021-11-08 16:09:07 +0900

    7_wordquiz(1)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,48 +1,85 @@
 package com.example.myapplication
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.ContextMenu
-import android.view.MenuItem
-import android.view.View
+import android.widget.Button
+import android.widget.RadioButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textView:TextView
+
+    private lateinit var wordTextView: TextView
+    private lateinit var radioButton1: RadioButton
+    private lateinit var radioButton2: RadioButton
+    private lateinit var radioButton3: RadioButton
+    private lateinit var radioButton4: RadioButton
+    private lateinit var nextButton: Button
+    private lateinit var prevButton: Button
+
+    private val wordBank = listOf<Word>(
+        Word("부주의한, 소홀한", "degenerate", "unity", "inadvertent", "array", 3),
+        Word("쇠약하게 하다", "vanity", "underhand", "enslave", "debilitate", 4),
+        Word("(위험·곤란)제거하다", "artisan", "sadistic", "glossy", "obviate", 4),
+        Word("우아한", "prostrate", "delude", "urbane", "renowned", 3),
+        Word("활기있게 하다", "bereave", "enliven", "occult", "besiege", 2)
+    )
+    private var curIndex:Int = 0;       //단어 인덱스 초기값
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView = findViewById(R.id.textView)
-        registerForContextMenu(textView)
-    }
 
-    override fun onCreateContextMenu(
-        menu: ContextMenu?,
-        v: View?,
-        menuInfo: ContextMenu.ContextMenuInfo?
-    ) {
-        super.onCreateContextMenu(menu, v, menuInfo)
-        menuInflater.inflate(R.menu.menu_main,menu)
-    }
+        wordTextView = findViewById(R.id.wordTextView)
+        radioButton1 = findViewById(R.id.radioButton1)
+        radioButton2 = findViewById(R.id.radioButton2)
+        radioButton3 = findViewById(R.id.radioButton3)
+        radioButton4 = findViewById(R.id.radioButton4)
+        updateWordQuiz()
 
-    override fun onContextItemSelected(item: MenuItem)=
-        when (item.itemId){
-            R.id.menu_yellow->{
-                textView.setBackgroundColor(Color.YELLOW)
-                true
+        nextButton = findViewById(R.id.nextButton)
+        nextButton.setOnClickListener {
+            curIndex = (curIndex + 1) % wordBank.size
+            updateWordQuiz()
+        }
+        prevButton = findViewById(R.id.prevButton)
+        prevButton.setOnClickListener {
+            if(curIndex == 0){
+                curIndex = wordBank.size - 1
+            }else{
+                curIndex -= 1
             }
-            R.id.menu_green->{
-                textView.setBackgroundColor(Color.GREEN)
-                true
-            }
-            R.id.menu_blue->{
-                textView.setBackgroundColor(Color.BLUE)
-                true
-            }
-            else->super.onContextItemSelected(item)
+            updateWordQuiz()
         }
 
-
+        radioButton1.setOnClickListener {
+            checkAnswer(1)
+        }
+        radioButton2.setOnClickListener {
+            checkAnswer(2)
+        }
+        radioButton3.setOnClickListener {
+            checkAnswer(3)
+        }
+        radioButton4.setOnClickListener {
+            checkAnswer(4)
+        }
+    }
+    private fun updateWordQuiz(){
+        wordTextView.text = wordBank[curIndex].question     //question 은 데이터 클래스 안에 프로퍼티
+        radioButton1.text = wordBank[curIndex].number_1
+        radioButton2.text = wordBank[curIndex].number_2
+        radioButton3.text = wordBank[curIndex].number_3
+        radioButton4.text = wordBank[curIndex].number_4
+    }
+    private fun checkAnswer(userAns:Int){
+        val correctAns = wordBank[curIndex].answer
+        val message = if(userAns == correctAns){
+            "정답입니다."
+        }else{
+            "오답입니다."
+        }
+        Toast.makeText(applicationContext,message,Toast.LENGTH_SHORT).show()
+    }
 }
 
---
+++ app/src/main/java/com/example/myapplication/Word.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+data class Word(val question: String,
+                val number_1: String,
+                val number_2: String,
+                val number_3: String,
+                val number_4: String,
+                val answer: Int)
