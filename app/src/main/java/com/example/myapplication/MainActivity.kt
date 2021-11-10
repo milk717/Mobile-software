43bf08d269326030233b8358adfcc51765aee20d
milk717 <robolindasoo@gmail.com>
2021-11-10 11:13:37 +0900

    7_Spinner_array 리소스 스타일 지정

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,19 +15,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //array 리소스 가져오는 법
-        val planets = resources.getStringArray(R.array.planets_array)
-        //array 리소스를 닫힌 스피너에 가져오는 아답터, 열린것도 가져오긴 하지만 엉성
-        val adapter = ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, planets)
-        //setDropDownViewResource 해야지 펼친것도 제대로 가져옴
+
+        //array 리소스의 스타일을 적용해서 불러오는 방법
+        val adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_item)
+
         adapter.setDropDownViewResource(
             android.R.layout.simple_spinner_dropdown_item
         )
 
         val spinner:Spinner = findViewById(R.id.spinner)
         spinner.adapter = adapter
-        object : AdapterView.OnItemSelectedListener{
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 //position 은 선택된 것의 인덱스
                 var planet = parent?.getItemAtPosition(position).toString()
@@ -38,7 +36,7 @@ class MainActivity : AppCompatActivity() {
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
             }
-        }.also { spinner.onItemSelectedListener = it }  //object 를 통해 만들어진 객체를 스피너에 할당해라, 만들어진 객체 == it
+        }
     }
 }
 
