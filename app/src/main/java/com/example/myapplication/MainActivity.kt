99022dac0ceaac227b4aba709250e5add0e86279
milk717 <robolindasoo@gmail.com>
2021-11-10 11:29:41 +0900

    7_ProgressBar

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,28 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val editText: EditText = findViewById(R.id.editText)
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
+        progressBar.progress = 0
 
-        //array 리소스의 스타일을 적용해서 불러오는 방법
-        val adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_item)
-
-        adapter.setDropDownViewResource(
-            android.R.layout.simple_spinner_dropdown_item
-        )
-
-        val spinner:Spinner = findViewById(R.id.spinner)
-        spinner.adapter = adapter
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                //position 은 선택된 것의 인덱스
-                var planet = parent?.getItemAtPosition(position).toString()
-                //parent?.context -> 부모는 메인 엑티비티
-                Toast.makeText(parent?.context, "선택한 행성은 $planet",Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            //앞뒤에 공백이 있을 때는 trim() 사용
+            var pval = Integer.parseInt(editText.text.toString().trim())
+            if (pval in 1..100)
+                progressBar.progress = pval
+            else {
+                progressBar.progress = 0
+                Toast.makeText(this, "값 설정 에러: $pval",
+                    Toast.LENGTH_SHORT).show()
             }
         }
+    }
+    private fun convertInt(s:String):Int {
+        return Integer.parseInt(s)
     }
 }
 
