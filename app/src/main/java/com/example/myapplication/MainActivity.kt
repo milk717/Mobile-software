771e4096053dc67bf9ebbfe1f7cfb9b56e1ab1c3
milk717 <robolindasoo@gmail.com>
2021-11-10 11:40:15 +0900

    7_SeekBar

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ProgressBar
+import android.view.WindowManager
+import android.widget.RatingBar
+import android.widget.SeekBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -14,25 +15,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editText: EditText = findViewById(R.id.editText)
-        val progressBar: ProgressBar = findViewById(R.id.progressBar)
-        progressBar.progress = 0
-
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
-            //앞뒤에 공백이 있을 때는 trim() 사용
-            var pval = Integer.parseInt(editText.text.toString().trim())
-            if (pval in 1..100)
-                progressBar.progress = pval
-            else {
-                progressBar.progress = 0
-                Toast.makeText(this, "값 설정 에러: $pval",
-                    Toast.LENGTH_SHORT).show()
-            }
+        val ratingBar: RatingBar = findViewById(R.id.ratingBar)
+        ratingBar.setOnRatingBarChangeListener{_, rating, _ ->
+            Toast.makeText(applicationContext, "점수:$rating",Toast.LENGTH_SHORT).show()
         }
+
+        val textView: TextView = findViewById(R.id.textView)
+        val seekBar: SeekBar = findViewById(R.id.seekBar)
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textView.text = "seek value = $progress"
+                setBrightness(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                textView.text = "seek value = ${seekBar?.progress}"
+            }
+        })
     }
-    private fun convertInt(s:String):Int {
-        return Integer.parseInt(s)
+    //밝기조절 함수
+    private fun setBrightness(b:Int){
+        var bright = b
+        if(bright < 10) bright = 10
+        else if(bright > 100) bright = 100
+        var lp = window.attributes  //윈도우가 갖고있는 값
+        lp.alpha = (bright.toFloat())/100F
+        lp.flags = (lp.flags or WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        window.attributes = lp
     }
 }
 
