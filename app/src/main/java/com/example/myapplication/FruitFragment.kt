71a73aebe13676a84583d742a5cad4bdfd935995
milk717 <robolindasoo@gmail.com>
2021-11-17 15:59:59 +0900

    9_RecyclerView_fragment

---
+++ app/src/main/java/com/example/myapplication/FruitFragment.kt
@@ -0,0 +1,52 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class FruitFragment : Fragment() {
+    private val fruitViewModel: FruitViewModel by lazy{
+        ViewModelProvider(this).get(FruitViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_fruit, container, false)
+        val fruits = fruitViewModel.fruitList
+        val recyclerView: RecyclerView = rootView.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = MyAdapter(fruits)
+        return rootView
+    }
+    class MyAdapter(var list:List<Fruit>) : RecyclerView.Adapter<MyViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val view = inflater.inflate(R.layout.item, parent,false)
+            return MyViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            val fruit = list[position]
+            holder.bind(fruit)
+        }
+
+        override fun getItemCount() = list.size
+    }
+    //화면에 띄워줄 리사이클러 뷰 정보를 가져오는 메소드
+    class  MyViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        var textView: TextView = view.findViewById(R.id.textView)
+
+        fun bind(fruit: Fruit){
+            textView.text = fruit.name
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity(){
@@ -20,34 +14,16 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fruits = fruitViewModel.fruitList
+        val currentFragment  = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if(currentFragment == null){
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container,FruitFragment())
+                .commit()
 
-        val recyclerView:RecyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = GridLayoutManager(this,2)
-        recyclerView.adapter = MyAdapter(fruits)
-    }
-
-    class MyAdapter(var list:List<Fruit>) : RecyclerView.Adapter<MyViewHolder>(){
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-            val inflater = LayoutInflater.from(parent.context)
-            val view = inflater.inflate(R.layout.item, parent,false)
-            return MyViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-            val fruit = list[position]
-            holder.bind(fruit)
-        }
-
-        override fun getItemCount() = list.size
-    }
-    //화면에 띄워줄 리사이클러 뷰 정보를 가져오는 메소드
-    class  MyViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        var textView: TextView = view.findViewById(R.id.textView)
-
-        fun bind(fruit: Fruit){
-            textView.text = fruit.name
         }
     }
+
+
 }
 
