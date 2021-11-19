14d9ed402c87ca5a626d33664969d2ad4b4f95b9
milk717 <robolindasoo@gmail.com>
2021-11-19 22:52:55 +0900

    10_DisplayMetrics

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,56 +1,30 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.view.ViewGroup
+import android.util.DisplayMetrics
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.databinding.ActivityMainBinding
-import com.example.myapplication.databinding.ItemBinding
 
 
-class MainActivity : AppCompatActivity(){
-    private lateinit var fruitModel:FruitViewModel
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        val binding: ActivityMainBinding =      //각각의 레이아웃의 루트를 가져옴
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        fruitModel = FruitViewModel()
-        binding.recyclerView.apply {
-            layoutManager = GridLayoutManager(context,3)
-            adapter = MyAdapter(fruitModel.fruits)
-        }
+        val textView: TextView = findViewById(R.id.textView)
+        val dm = resources.displayMetrics
+        val str = """
+            widthPixels = ${dm.widthPixels}
+            heightPixels = ${dm.heightPixels}
+            density = ${dm.density}
+            scaledDensity = ${dm.scaledDensity}
+            densityDpi = ${dm.densityDpi}
+            DENSITY_DEFAULT = ${DisplayMetrics.DENSITY_DEFAULT}            
+            xdpi = ${dm.xdpi}
+            ydpi = ${dm.ydpi}
+            """.trimIndent()
+
+        textView.text = str
     }
-
-    class  MyViewHolder(private val binding:ItemBinding) :
-        RecyclerView.ViewHolder(binding.root){
-        init{
-            binding.viewModel = FruitViewModel()
-        }
-        fun bind(fruit:Fruit){
-            binding.viewModel?.fruit = fruit
-            binding.executePendingBindings()
-        }
-    }
-
-    inner class MyAdapter(private var fruits:List<Fruit>) : RecyclerView.Adapter<MyViewHolder>(){
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-            val binding = DataBindingUtil.inflate<ItemBinding>(
-                layoutInflater, R.layout.item, parent, false)
-            return MyViewHolder(binding)
-        }
-
-        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-            val fruit = fruits[position]
-            holder.bind(fruit)
-        }
-
-        override fun getItemCount() = fruits.size
-    }
-
-
 }
