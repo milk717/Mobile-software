1ad3d76f30c84d87b8ced3d7fa4372098d0a6507
milk717 <robolindasoo@gmail.com>
2021-12-01 14:21:57 +0900

    11_이미지 회전 – matrix 사용

--- app/src/main/java/com/example/myapplication/MyView.kt
+++ app/src/main/java/com/example/myapplication/MyView.kt
@@ -14,30 +14,53 @@ class MyView(context:Context): View(context) {
             w, h, Bitmap.Config.ARGB_8888)
         myCanvas = Canvas(myBitmap)
     }
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         myCanvas.drawColor(Color.BLUE)
 
-        drawMirrorBitmap()
+        drawRotatedBitmap()
         canvas?.drawBitmap(myBitmap, 0f, 0f, null)
     }
-    private fun drawMirrorBitmap() {
-        var bobBitmap: Bitmap = BitmapFactory.decodeResource(
-            resources, R.drawable.bob)
 
-        myCanvas.drawBitmap(bobBitmap, 25f, 25f, null)
-        val matrix = Matrix()
-        matrix.preScale(1f, -1f)
+    private fun drawRotatedBitmap() {
+        var bobBitmap: Bitmap = BitmapFactory.decodeResource(
+            resources, R.drawable.bob
+        )
+
         val w = bobBitmap.width
         val h = bobBitmap.height
-        var bobBitmap2 = Bitmap.createBitmap(bobBitmap,
-            0, 0, w, h, matrix, false)
+        var rotatedBitmap: Bitmap? = null
 
-        myCanvas.drawBitmap(bobBitmap2, 25f, h+50f, null)
-        matrix.preRotate(-90f)
-        var bobBitmap3 = Bitmap.createBitmap(bobBitmap,
-            0, 0, w, h, matrix, false)
-        myCanvas.drawBitmap(bobBitmap3, 25f, 2*h+50f, null)
+        var rotation = 0f
+        var horizontalPos = 50f
+        var verticalPos = 25f
+        val matrix = Matrix()
+
+        while (rotation < 360) {
+            matrix.reset()
+
+            // M' = M*R(degrees), 시계방향 회전
+            matrix.preRotate(rotation)
+
+            // 원점(0,0), 너비(w), 높이(h)
+            // 위치 이동만이 아닌 다른 변환도 포함되었을 경우에만 true 설정
+            rotatedBitmap = Bitmap.createBitmap(
+                bobBitmap, 0, 0, w, h, matrix, true
+            )
+
+            // 이미지 출력 위치(horizontalPos, verticalPos)
+            // 회전할 때마다 오른쪽으로 120, 아래쪽으로 70씩 이동
+            myCanvas.drawBitmap(
+                rotatedBitmap,
+                horizontalPos, verticalPos, null
+            )
+
+            horizontalPos += w
+            verticalPos += h
+            rotation += 30f
+        }
     }
 }
+
 
