1e3e59a91cfd230f0005657543c02eacbaddb020
milk717 <robolindasoo@gmail.com>
2021-12-01 14:20:57 +0900

    11_이미지 상하반전 – matrix 사용

--- app/src/main/java/com/example/myapplication/MyView.kt
+++ app/src/main/java/com/example/myapplication/MyView.kt
@@ -1,15 +1,12 @@
 package com.example.myapplication
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Color
+import android.graphics.*
 import android.view.View
 
 class MyView(context:Context): View(context) {
-    lateinit var myBitmap: Bitmap
-    lateinit var myCanvas: Canvas
+    lateinit var myBitmap:Bitmap
+    lateinit var myCanvas:Canvas
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -17,23 +14,30 @@ class MyView(context:Context): View(context) {
             w, h, Bitmap.Config.ARGB_8888)
         myCanvas = Canvas(myBitmap)
     }
-
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         myCanvas.drawColor(Color.BLUE)
 
-        drawEnlargedBitmap()
+        drawMirrorBitmap()
         canvas?.drawBitmap(myBitmap, 0f, 0f, null)
     }
-
-    private fun drawEnlargedBitmap() {
+    private fun drawMirrorBitmap() {
         var bobBitmap: Bitmap = BitmapFactory.decodeResource(
             resources, R.drawable.bob)
-        myCanvas.drawBitmap(bobBitmap, 25f, 25f, null)
 
-        bobBitmap = Bitmap.createScaledBitmap(bobBitmap,
-            300, 400, false)
+        myCanvas.drawBitmap(bobBitmap, 25f, 25f, null)
+        val matrix = Matrix()
+        matrix.preScale(1f, -1f)
         val w = bobBitmap.width
-        myCanvas.drawBitmap(bobBitmap, w+25f, 25f, null)
+        val h = bobBitmap.height
+        var bobBitmap2 = Bitmap.createBitmap(bobBitmap,
+            0, 0, w, h, matrix, false)
+
+        myCanvas.drawBitmap(bobBitmap2, 25f, h+50f, null)
+        matrix.preRotate(-90f)
+        var bobBitmap3 = Bitmap.createBitmap(bobBitmap,
+            0, 0, w, h, matrix, false)
+        myCanvas.drawBitmap(bobBitmap3, 25f, 2*h+50f, null)
     }
 }
+
