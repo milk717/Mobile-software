d02907ac6f03840ea8c32032f40bcbbc0a1825da
milk717 <robolindasoo@gmail.com>
2021-12-01 12:46:02 +0900

    11_외부 font

--- app/src/main/java/com/example/myapplication/MyView.kt
+++ app/src/main/java/com/example/myapplication/MyView.kt
@@ -11,23 +11,19 @@ class MyView(context: Context): View(context) {
         super.onDraw(canvas)
         if (canvas == null) return
         canvas.drawRGB(255, 255, 0)
+
         val paint = Paint()
         paint.textSize = 50f
 
-        var t = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
-        paint.typeface = t
-        canvas.drawText("DEFAULT 폰트", 10f, 100f, paint)
-
-        t = Typeface.create(Typeface.MONOSPACE, Typeface.BOLD)
-        paint.typeface = t
-        canvas.drawText("MONOSPACE 폰트", 10f, 200f, paint)
-
-        t = Typeface.create(Typeface.SERIF, Typeface.BOLD_ITALIC)
-        paint.typeface = t
-        canvas.drawText("SERIF 폰트", 10f, 300f, paint)
-
-        t = Typeface.create(Typeface.SANS_SERIF, Typeface.ITALIC)
-        paint.typeface = t
-        canvas.drawText("SANS_SERIF 폰트", 10f, 400f, paint)
+        try {
+            val myFont = Typeface.createFromAsset(
+                context.assets, "ScriptoniteDemo.ttf"
+            )
+            paint.typeface = myFont
+            canvas.drawText("This is a new font.", 10f, 100f, paint)
+            canvas.drawText("Have fun!", 10f, 200f, paint)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
