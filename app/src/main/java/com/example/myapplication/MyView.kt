b43122405c6654a53da7798bf75896b2f5a2488d
milk717 <robolindasoo@gmail.com>
2021-12-01 13:19:24 +0900

    11_이미지 리소스를 비트맵 객체로 변환

--- app/src/main/java/com/example/myapplication/MyView.kt
+++ app/src/main/java/com/example/myapplication/MyView.kt
@@ -1,28 +1,29 @@
 package com.example.myapplication
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Typeface
+import android.graphics.*
 import android.view.View
 
 class MyView(context: Context): View(context) {
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         if (canvas == null) return
-        canvas.drawRGB(255, 255, 0)
+
+        canvas.drawColor(Color.LTGRAY)
 
         val paint = Paint()
-        paint.textSize = 50f
+        val b:Bitmap = BitmapFactory.decodeResource(resources,
+            R.drawable.harubang)
+        canvas.drawBitmap(b,0f,0f,null)
 
-        try{
-            val myFont = Typeface.createFromAsset(
-                context.assets,"ScriptoniteDemo.ttf")   //context가 asstes을 관리함, 폰트이름 정확히
-            paint.typeface = myFont
-            canvas.drawText("This is a new font.", 10f, 100f, paint)
-            canvas.drawText("Have fun!", 10f, 200f, paint)
-        }catch (e: Exception){
-            e.printStackTrace()
-        }
+        paint.textSize = 50f
+        var px = 500f
+        var py = 100f
+        canvas.drawText(width.toString(), px, py, paint)
+        canvas.drawText(height.toString(), px+150f, py, paint)
+
+        py += 100f
+        canvas.drawText(b.width.toString(), px, py, paint)
+        canvas.drawText(b.height.toString(), px+150f, py, paint)
     }
 }
