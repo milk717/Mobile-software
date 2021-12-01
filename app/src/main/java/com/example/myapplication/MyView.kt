0b778f6dd217783edc1da0466c386b83d1b15747
milk717 <robolindasoo@gmail.com>
2021-12-01 13:44:38 +0900

    11_비트맵 공간을 만든 후 이 공간에 비트맵 이미지 그리기 - onDraw

--- app/src/main/java/com/example/myapplication/MyView.kt
+++ app/src/main/java/com/example/myapplication/MyView.kt
@@ -1,31 +1,34 @@
 package com.example.myapplication
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
 import android.view.View
 
 class MyView(context: Context): View(context) {
     private val b: Bitmap = BitmapFactory.decodeResource(
         resources, R.drawable.harubang)
-    private val w:Int = b.width // 하루방 bitmap 너비
-    private val h:Int = b.height // 하루방 bitmap 높이
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         if (canvas == null) return
         canvas.drawColor(Color.LTGRAY)
 
-        canvas.drawBitmap(b, 0f, 0f, null)
+        //비트맵 공간을 만듬.
+        val mBitmap = Bitmap.createBitmap(600,600,
+            Bitmap.Config.ARGB_8888)
+        //아까 만든 비트맵 공간을 캔버스에 넣음?
+        val mCanvas = Canvas(mBitmap)
+        mCanvas.drawColor(Color.YELLOW)
 
-        // 하루방 오른쪽 눈 부분
-        val src = Rect(140, 60, 180, 150)
+        //노란색 mCanvas 안에서의 돌하르방 위치
+        //노란색 mCanvas 안에 돌하르방을 그림
+        mCanvas.drawBitmap(b,20f,20f,null)
 
-        var pi = w + 50
-        val dst = Rect(pi, 0, pi + w, h)
-        canvas.drawBitmap(b, src, dst, null)
-
-        var pj = h + 50
-        val dst2 = Rect(pi, pj, pi + w/2, pj + h/2)
-        canvas.drawBitmap(b, src, dst2, null)
+        //회색 전체 캔버스에서 노란색 mCanvas 의 위치
+        //전체 캔버스 안에 노란색 mCanvas 를 그림
+        canvas.drawBitmap(mBitmap,50f,50f,null)
     }
 }
