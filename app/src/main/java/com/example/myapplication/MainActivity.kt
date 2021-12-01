e4d2b0d26775290e819416b179591da298ba3c08
milk717 <robolindasoo@gmail.com>
2021-12-01 12:34:03 +0900

    11_custom view를 XML layout 파일에서 참조

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,8 +8,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        setContentView(MyView(this))
+        setContentView(R.layout.activity_main)
+        //setContentView(MyView(this))
     }
 
 }
--- app/src/main/java/com/example/myapplication/MyView.kt
+++ app/src/main/java/com/example/myapplication/MyView.kt
@@ -5,36 +5,31 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
+import android.util.AttributeSet
 import android.view.View
 
-class MyView(context: Context) : View(context) {
-    init {
-        setBackgroundColor(Color.BLUE)
+class MyView : View {
+    constructor(context: Context): super(context)  {
+        setBackgroundColor(Color.YELLOW)
+    }
+    //코틀린 코드에서 만든 것을 xml 에 포함시킬 때 반드시 필요한 생성자
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs)  {
+        setBackgroundColor(Color.YELLOW)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         if (canvas == null) return
+
         val paint = Paint()
-        val r1 = RectF(10f, 10f, 110f, 110f)
-        val r2 = RectF(150f, 10f, 350f, 100f)
-        val r3 = RectF(10f, 120f, 110f, 220f)
+        val r = RectF(100f, 100f, 300f, 300f)
 
-        paint.color = Color.YELLOW
-        //canvas.drawRoundRect(r1, 5f, 5f, paint)
-        canvas.drawRect(r1,paint)
-
-        canvas.drawOval(r2, paint)
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 10f
+        canvas.drawRect(r, paint)
 
         paint.color = Color.RED
-        //호그리기, true 는 호의 끝부분이 중심이랑 연결됨
-        canvas.drawArc(r3, 120f, 270f, true, paint)
-
-        paint.color = Color.YELLOW
-        paint.strokeWidth = 10f
-        val pts = floatArrayOf(10f, 250f, 100f, 270f,
-            100f, 270f, 20f, 290f,
-            20f, 290f, 120f, 300f)
-        canvas.drawLines(pts, paint)
+        paint.style = Paint.Style.FILL
+        canvas.drawArc(r, 90f, 270f, false, paint)
     }
 }
