4b44739b1bb0ea84dc3a00b2d8696a3e739953fc
milk717 <robolindasoo@gmail.com>
2021-12-05 18:31:08 +0900

    wordQuiz_내가짠거

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,29 +1,105 @@
 package com.example.myapplication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
+const val EXTRA_MESSAGE = "com.example.myapplication.message"
+const val RETURN_CHECK = "com.example.myapplication.returnMessage"
 
 class MainActivity : AppCompatActivity() {
+    private var hintCheck = 0
+    private lateinit var wordTextView:TextView
+    private lateinit var radioButton1:RadioButton
+    private lateinit var radioButton2:RadioButton
+    private lateinit var radioButton3:RadioButton
+    private lateinit var radioButton4:RadioButton
+    private lateinit var hintTextView:TextView
+
+    private val movieViewModel: WordViewModel by lazy{
+        ViewModelProvider(this).get(WordViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val radioGroup: RadioGroup = findViewById(R.id.radioGroup)
+        val prevButton: Button = findViewById(R.id.prevButton)
+        val nextButton: Button = findViewById(R.id.nextButton)
+        val hintButton: Button = findViewById(R.id.hintButton)
+        hintTextView = findViewById(R.id.hintTextView)
 
-        //동적 교체할때는 필요한 코드지만 그냥 하나만 띄워줄땐 이거 없어도 실행은 됨. 왜냐면 xml 파일에서 넣어줬기때문
-        val currentFragment  = supportFragmentManager.findFragmentById(R.id.fragment_container)
-        if(currentFragment == null){    //현재 프래그먼트가 비어 있을 때
-            supportFragmentManager      //프래그먼트를 호출하는 매니저
-                .beginTransaction()                             //일련의 과정을 실행하라
-                .add(R.id.fragment_container,MovieFragment())   //프래그먼트 들어갈곳 == 컨테이너에 생성한 객체 ==MovieFragment 넣어줘라
-                .commit()                                       //마무리?
+        wordTextView = findViewById(R.id.textView)
+        radioButton1= findViewById(R.id.radioButton1)
+        radioButton2= findViewById(R.id.radioButton2)
+        radioButton3= findViewById(R.id.radioButton3)
+        radioButton4= findViewById(R.id.radioButton4)
+
+        question()
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            var id = 1
+            when(checkedId){
+                R.id.radioButton1-> id = 1
+                R.id.radioButton2-> id = 2
+                R.id.radioButton3-> id = 3
+                R.id.radioButton4-> id = 4
+                else -> 1
+            }
+            if(id == movieViewModel.wordA){
+                Toast.makeText(applicationContext,"정답입니다",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(applicationContext,"오답입니다",Toast.LENGTH_SHORT).show()
+            }
+        }
+        prevButton.setOnClickListener{
+            movieViewModel.previous()
+            hintTextView.visibility = View.INVISIBLE
+            question()
+        }
+        nextButton.setOnClickListener {
+            movieViewModel.next()
+            hintTextView.visibility = View.INVISIBLE
+            question()
+        }
+
+        hintButton.setOnClickListener {
+            var i: Intent = HintActivity.newIntent(this,movieViewModel.curIndex)
+            startForResult.launch((i))
         }
     }
-    fun showToast(text: String){
-        Toast.makeText(applicationContext,text, Toast.LENGTH_SHORT).show()
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {      //엑티비티 결과값이 ok이고
+            if (result.data != null) {  //인텐트 자체도 null 이 아닐 때
+                val extras: Bundle? = result.data?.extras
+                //Elvis 연산자 사용
+                hintCheck = extras?.getInt(RETURN_CHECK,0)?:0
+                if(hintCheck == 1){
+                    hintTextView.visibility = View.VISIBLE
+                }
+                else{
+                    hintTextView.visibility = View.INVISIBLE
+                }
+            }
+        }
     }
-
+    private fun question(){
+        wordTextView.text = movieViewModel.wordQ
+        radioButton1.text = (movieViewModel.word1)
+        radioButton2.text = (movieViewModel.word2)
+        radioButton3.text = (movieViewModel.word3)
+        radioButton4.text = (movieViewModel.word4)
+        radioButton1.isChecked = false
+        radioButton2.isChecked = false
+        radioButton3.isChecked = false
+        radioButton4.isChecked = false
+    }
 }
-
--- app/src/main/java/com/example/myapplication/MovieItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication
-
-data class MovieItem(var title: String,
-                    var rating: String,
-                    var genre: String,
-                    var year: String,
-                    var resId: Int)
