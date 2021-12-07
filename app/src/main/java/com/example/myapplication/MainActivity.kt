728e35bdc890b5a68ac954dd6078399e902cddf9
milk717 <robolindasoo@gmail.com>
2021-12-07 19:48:09 +0900

    13_Frame Animation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -9,6 +12,18 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        val startButton = findViewById<Button>(R.id.startButton)
+        val stopButton = findViewById<Button>(R.id.stopButton)
+        val rocketAnim: AnimationDrawable = imageView.background as AnimationDrawable
+
+        startButton.setOnClickListener {
+            rocketAnim.start()
+        }
+        stopButton.setOnClickListener {
+            rocketAnim.stop()
+        }
     }
 
 
