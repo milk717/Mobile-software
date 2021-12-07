96f209b7c7c63873f18da36580ab74f02d605ab5
milk717 <robolindasoo@gmail.com>
2021-12-07 20:28:15 +0900

    13_Property Animation_X값

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.widget.Button
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -12,29 +8,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(MyView(this))
 
-        val rect_image = findViewById<ImageView>(R.id.rect_image)
-        val translate = findViewById<Button>(R.id.translate)
-        val rotate = findViewById<Button>(R.id.rotate)
-        val scale = findViewById<Button>(R.id.scale)
-        val alpha = findViewById<Button>(R.id.alpha)
-
-        translate.setOnClickListener {
-            var anim: Animation = AnimationUtils.loadAnimation(this, R.anim.translate)
-            rect_image.startAnimation(anim)
-        }
-        rotate.setOnClickListener {
-            var anim: Animation = AnimationUtils.loadAnimation(this, R.anim.rotate)
-            rect_image.startAnimation(anim)
-        }
-        scale.setOnClickListener {
-            var anim: Animation = AnimationUtils.loadAnimation(this, R.anim.scale)
-            rect_image.startAnimation(anim)
-        }
-        alpha.setOnClickListener {
-            var anim: Animation = AnimationUtils.loadAnimation(this, R.anim.alpha)
-            rect_image.startAnimation(anim)
-        }
     }
 }
--- app/src/main/java/com/example/myapplication/MyView.kt
+++ app/src/main/java/com/example/myapplication/MyView.kt
@@ -1,34 +1,41 @@
-//package com.example.myapplication
-//
-//import android.content.Context
-//import android.graphics.Canvas
-//import android.graphics.Color
-//import android.graphics.Paint
-//import android.graphics.RectF
-//import android.util.AttributeSet
-//import android.view.View
-//
-//class MyView:View{
-//    constructor(context: Context): super(context)  {
-//        setBackgroundColor(Color.YELLOW)
-//    }
-//    //코틀린 코드에서 만든 것을 xml 에 포함시킬 때 반드시 필요한 생성자
-//    constructor(context: Context, attrs: AttributeSet): super(context, attrs)  {
-//        setBackgroundColor(Color.YELLOW)
-//    }
-//    override fun onDraw(canvas: Canvas?) {
-//
-//        super.onDraw(canvas)
-//        if(canvas == null) return       //캔버스객체가 null이 아니어야 함
-//        val paint = Paint()
-//
-//        val r = RectF(100f,100f,300f,300f)
-//
-//        paint.style = Paint.Style.STROKE
-//        canvas.drawRect(r,paint)
-//
-//        paint.color = Color.RED
-//        paint.style = Paint.Style.FILL
-//        canvas.drawArc(r,90f,270f,false,paint)
-//    }
-//}
+package com.example.myapplication
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.MotionEvent
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+
+const val RADIUS = 100f
+class MyView(context: Context):View(context){
+  private var mx:Float = RADIUS
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        canvas?.drawColor(Color.YELLOW) //배경색 지정
+
+        val paint = Paint()
+        paint.color = Color.RED
+        canvas?.drawCircle(mx,300f,RADIUS,paint)        //원 그리기
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if(event?.action == MotionEvent.ACTION_DOWN){   //터치 눌렸을 때
+            val valueAnim = ValueAnimator.ofFloat(RADIUS,width- RADIUS)
+            valueAnim.apply {
+                duration = 2000
+                interpolator = AccelerateInterpolator()
+                start()
+            }
+            valueAnim.addUpdateListener { animation ->
+                mx = animation?.animatedValue as Float
+                invalidate()
+            }
+            return true
+        }
+        return super.onTouchEvent(event)
+    }
+}
