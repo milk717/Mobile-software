605172e7c8e5d3dc4d58d029c6abd757e910e769
milk717 <robolindasoo@gmail.com>
2021-12-07 19:59:23 +0900

    13_Tween animation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,30 +1,40 @@
 package com.example.myapplication
 
-import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageView = findViewById<ImageView>(R.id.imageView)
-        val startButton = findViewById<Button>(R.id.startButton)
-        val stopButton = findViewById<Button>(R.id.stopButton)
-        val rocketAnim: AnimationDrawable = imageView.background as AnimationDrawable
+        val rect_image = findViewById<ImageView>(R.id.rect_image)
+        val translate = findViewById<Button>(R.id.translate)
+        val rotate = findViewById<Button>(R.id.rotate)
+        val scale = findViewById<Button>(R.id.scale)
+        val alpha = findViewById<Button>(R.id.alpha)
 
-        startButton.setOnClickListener {
-            rocketAnim.start()
+        translate.setOnClickListener {
+            var anim: Animation = AnimationUtils.loadAnimation(this, R.anim.translate)
+            rect_image.startAnimation(anim)
         }
-        stopButton.setOnClickListener {
-            rocketAnim.stop()
+        rotate.setOnClickListener {
+            var anim: Animation = AnimationUtils.loadAnimation(this, R.anim.rotate)
+            rect_image.startAnimation(anim)
+        }
+        scale.setOnClickListener {
+            var anim: Animation = AnimationUtils.loadAnimation(this, R.anim.scale)
+            rect_image.startAnimation(anim)
+        }
+        alpha.setOnClickListener {
+            var anim: Animation = AnimationUtils.loadAnimation(this, R.anim.alpha)
+            rect_image.startAnimation(anim)
         }
     }
-
-
 }
